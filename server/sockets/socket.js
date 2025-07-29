f98802356b46a97148edcf33c535ed2cb901db94
Jacob Witty <128905829+jacobkau@users.noreply.github.com>
2025-07-29 15:30:05 +0300

    Update socket.js
    configured to the live system

--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -5,7 +5,7 @@ let io;
 export const initSocket = (server) => {
   io = new Server(server, {
     cors: {
-      origin: ["http://localhost:3000", "http://localhost:5173"],
+      origin: ["http://localhost:3000", "http://localhost:5173", "https://wittyhandupsystem.vercel.app/"],
       methods: ["GET", "POST"],
         credentials: true
     }
