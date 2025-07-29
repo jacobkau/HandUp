a9f316890206d83daf3098ffaea7b0346e50662b
Jacob Witty <128905829+jacobkau@users.noreply.github.com>
2025-07-29 15:45:16 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_ORIGIN || 'https://wittyhandupsystem.vercel.app/',
+  origin: process.env.CLIENT_ORIGIN || 'https://wittyhandupsystem.vercel.app',
   credentials: true,
 }));
 app.use(express.json());
